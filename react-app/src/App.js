3bb07e3f11276a81ec5f24431494223b1baf1438
fabaneon <fabaneon@gmail.com>
2022-09-04 08:05:30 +0900

    Update App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   render()  {
     return  (
       <div className="App">
-        Hello, React!!
+      <h1 id = "title"><u>Welcome to my React_Study !</u></h1>
+      <h3>환영합니다. 아래 목차를 통해서 React 학습 결과물을 열람해볼 수 있습니다 :)</h3>
+      <h5>추신. 단기 Toy Project.</h5>
 
       </div>
     );
