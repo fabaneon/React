c5d6d847cb856f0c5a9968e4a58c05cc1d4e9ee6
fabaneon <fabaneon@gmail.com>
2022-09-03 23:16:28 +0900

    2022-09-03 클래스 방식으로 변경
    기존 함수 방식에서 클래스 방식으로 변경함.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,30 +1,15 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>환영합니다.</h1>
-          이곳은 박상우의 React 학습 페이지입니다.
-        </p>
+class App extends Component {
+  render()  {
+    return  (
+      <div className="App">
 
-        <p>
-          <code>src/App.js</code> 를 수정하고 저장하여 출력합니다.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      </div>
+    );
+  }
 }
 
 export default App;
