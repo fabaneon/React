ed3eec506f49dec9f8daa556960404cdb70bde3e
fabaneon <fabaneon@gmail.com>
2023-01-08 06:37:08 +0000

    2023-01-08 커뮤니티로 개편 + CRUD 전 기능 구현 ++ 개행 방법 공부 [하단 참고] +++ REACT는 CSS white-space 속성을 이용.       텍스트가 화면상 출력되는 태그에 적용.     JSX에서 \n 개행문자를 인식할 수 있게 해주는 +++ 다른 라이브러리, 프레임워크에선 그냥 replace로 {66번줄 참고}
    어제 상태가 상당히 안좋았었나보다.
    스파게티코드에 이해도 안되는 로직 덩어리라서
    그냥 다 갈어엎고 다시 만들었다.
    
    새로하는김에 입력된 문자열의 개행 방법도 공부했고
    예에에전에 귀찮아서 안만든 수정, 삭제기능까지 구현했다
    
    다음은 Ajax 해볼까

--- react-app/src/Component/posting/posting.js
+++ react-app/src/Component/posting/posting.js
@@ -1,73 +1,166 @@
-import react,{useState} from 'react';
+import {useState} from 'react';
 import "./posting.css";
 
+function SearchRead(props) {
+	var mode = props.mode;
+	var Id = props.id;
+	var List = props.postList;
+	var post = [];
+	var Edit = [];
+		
+	if(List.length < 1){
+		post = <p>첫 글을 작성해주세요!</p>
+	}
+	else if(mode === "read"){
+		post.push(<div key={List[Id].id + List[Id].title} className="Orginal-Post">
+					  <h1 className="title">{List[Id].title}</h1>
+					  <p className="Posted-article">{List[Id].content}</p></div>
+		);
+		Edit.push(<> {"|"} <input key={"수정버튼"} type="button" defaultValue="수정" 
+							   onClick={(event)=>{
+						  event.preventDefault();
+						  props.onEditmode();
+					  }}></input>{"|"}
+				 <input key={"뒤로가기버튼"}type="button" defaultValue="뒤로가기" 
+					 onClick={(event)=>{
+						  event.preventDefault();
+						  props.onSearchmode();
+					  }}></input>
+					 <input key={"삭제버튼"}type="button" defaultValue="삭제" 
+					 onClick={(event)=>{
+						  event.preventDefault();
+						  props.del();
+					  }}></input></>
 
-function Reading(props){
-	var posts = props.post;
-	console.log(props.post);
-	var post = document.querySelector(".Orginal-Post");
-	console.log(post);
-	post.innerHTML = posts;
-	return(
-	<>
-		<div className="Article-Top-OptionBar">
-			<input type="button" name="editbutton" value="수정" onClick={(event)=>{
-									event.preventDefault();
-									props.onChangemode();							
-								}}/>
-
-		</div>
-		<div className="Original-Post">
-		</div>
-	</>	
-	);
-}
-
-function Editing(props){
+				 );
+	}
+	else{
+		post = 
+			<>
+				<h1 className="title">Community</h1><p>자유롭게 활동해보세요</p>
+			</>;
+	}
 	
 	return(
 		<>
-			<label onSubmit={(event,props)=>{
-				event.preventDefault();
-				
-			}}>
-				<div className="Article-Top-OptionBar">
-					<input  type="submit" name="submitbutton" value="게시"/>
-					<input key="post-reading" type="button" name="canclebutton" value="취소" onClick={(event)=>{
-									event.preventDefault();
-									props.onChangemode();
-
-									}}/>
-				</div>
-
-				<input className="Article-Edit" type="text-area"></input>
-			</label>
+			
+			<div className="Article-Top-OptionBar">
+				<input type="button" defaultValue="작성" onClick={(event)=>{
+					event.preventDefault();
+					props.onWritemode();
+			}}/>
+				{Edit}
+			</div>
+			{post}
 		</>
 	);
 }
 
-function Posting(){
-	const [posting,postingMode] = useState("reading");
-	const [post,changePost] = [<><p>
-				본문입니다.<br/>
-				이러한 내용이죠<br/>
-				그런데 사실 테스트중이라 별 내용은 없습니다.
-			</p>
-			<h1>안녕히 계세요 ^^</h1></>];
-	let postView, postingfooter;
+function Write (props) {
+	return(
 	
-	if(posting === "reading"){
-		postView = <Reading post={post} onChangemode={()=>{postingMode("editing");
+		<form onSubmit={(event)=>{
+				event.preventDefault();
+				let title = event.target.title.value;
+				let content = event.target.content.value;
+				/*content = content.replace(/(?:\r\n|\r|\n)/g, '<br/>');*/
+				//이건 리액트에선 안먹힌단다.
+				props.Write(title,content);
+			}}>
 			
-											  }}/>
+			<div className="Article-Top-OptionBar">
+				<input type="submit" defaultValue="게시"/> |
+				<input type="button" defaultValue="취소" onClick={(event)=>{
+					event.preventDefault();
+					props.onSearchmode();
+				}}></input>
+
+			</div>
+			<input required className ="Article-write-title" name = "title"  placeholder="제목"/>
+			<textarea required className="Article-write-content" name = "content" placeholder="내용"/>
+		</form>
+	
+	);
+}
+
+function Edit (props){
+	var id = props.selectedId;
+	var List = props.postList;
+	var prevtitle = List[id].title;
+	var prevcontent = List[id].content;
+	return(
+		<form onSubmit={(event)=>{
+				event.preventDefault();
+				let title = event.target.title.value;
+				let content = event.target.content.value;
+				/*content = content.replace(/(?:\r\n|\r|\n)/g, '<br>');*/
+				//이건 리액트에선 안먹힌단다.
+				props.Edit(title,content);
+			}}>
+			
+			<div className="Article-Top-OptionBar">
+				<input type="submit" defaultValue="게시"/> |
+				<input type="button" defaultValue="취소" onClick={(event)=>{
+					event.preventDefault();
+					props.onSearchmode();
+				}}></input>
+
+			</div>
+			<input required className ="Article-write-title" name = "title"  defaultValue={prevtitle}/>
+			<textarea required className="Article-write-content" name = "content" defaultValue={prevcontent}/>
+		</form>
+	);
+}
+function Posting(){
+	var postView = [];
+	const [postList, setpostList] = useState([]);
+	const [mode,setMode] = useState("search");
+	const [selectedId,selectId] = useState(0);
+	const [nextId,setNextId] = useState(0);
+	let nav = [];
+	
+	if(postList.length > 0){	
+		for(var i=0; i<postList.length; i++){
+			let a = postList[i];
+			nav.push(<li key={a.id}><a key={a.id+a.title}href={'/read/'+a.id} onClick={(event)=>{
+								 event.preventDefault();
+								 selectId(a.id);	 
+								 setMode("read");
+							 }}>{a.title}</a></li>);
+		}
 	}
-	else if(posting === "editing"){
-		postView = <Editing post={[post]} onChangemode={()=>{postingMode("reading");
-											  }}/>
+	if(mode === "delete"){
+		var delList =  postList;
+		delList = delList.splice(selectedId, 1);
+		setpostList(delList);
+		setMode("search");
+	}
+	else if(mode === "search" || mode === "read"){
+		postView = <SearchRead id={selectedId} postList={postList} mode={mode} onSearchmode={()=>{setMode("search");}} 
+					   onWritemode={()=>{setMode("write")}} onEditmode={()=>{setMode("edit");}} del={()=>{setMode("delete");}}/>
+	}
+	else if(mode === "write"){
+		postView = <Write onSearchmode={()=>{setMode("search");}}
+					   Write={(write_title,write_content)=>{
+				var newpost = {id:nextId, title:write_title, content: write_content};
+				var newpostList = postList;
+				newpostList.push(newpost);
+				setpostList(newpostList);
+				setNextId(nextId+1);
+				selectId(nextId);
+				setMode("read");
+			}} />
 		
 	}
-	
-	
+	else if(mode === "edit"){
+		postView = <Edit selectedId={selectedId} postList={postList} Edit={(edit_title,edit_content)=>{
+				var newpost = {id:selectedId, title:edit_title, content: edit_content};
+				var newpostList = postList;
+				newpostList[selectedId] = newpost;
+				setpostList(newpostList);
+				setMode("read");
+			}} />
+	}
 	return(
 		<div className="Container-App-Main">
 			<div className="App-header">
@@ -75,7 +168,9 @@ function Posting(){
 			</div>
 			<div className="App-Main">
 				<div className="Navigator">
-					<li>링크 리스트</li>
+					<ul>
+						{nav}
+					</ul>
 				</div>
 				<div className="Article">
 					{postView}
