f71df72e69f1a5f684133a7dff3b11aad7d2476e
fabaneon <fabaneon@gmail.com>
2023-01-29 01:55:48 +0000

    2023-01-29 Python 섹션 추가 우선 섹션만 추가해둔다.

--- new-profile-web/src/index.js
+++ new-profile-web/src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter,Route,Routes,NavLink} from 'react-router-dom';
 
 import App from './profilemain';
 import Graphicdesign from './webgraphicmain';
-
+import Python from './pythonmain';
 import reportWebVitals from './reportWebVitals';
 
 import './CSS/BasicDesign.css';
@@ -13,14 +13,48 @@ var lact = <>
 <span id='NavBtn'><NavLink to={"/Portfolio-Profile_Website/"}>로비</NavLink></span>
 <span id='NavBtn'><NavLink to={"/Portfolio-Profile_Website/main"}>프로필</NavLink></span>
 <span id='NavBtn'><NavLink to={"/Web-Motion-Graphic/index"}>웹 모션 그래픽</NavLink></span>
+<span id='NavBtn'><NavLink to={"/Python/index"}>Python</NavLink></span>
 </>
 
 function LobbyIndex(){
 	return(
-	<>
-		로비 페이지는 현재 준비중입니다.<br/>
-		위 버튼을 눌러 다른 페이지를 탐색해보세요!
-	</>
+	<div id="App-Profile-main">
+		<div id="App-Lobby">
+			<p id="line">- - - -</p>
+			<p className="title">
+				개요
+			</p>
+			<p className="many-article">
+				본 웹페이지는 저의 SW 지식과 학습의 결과물을 시각자료로 표현합니다.
+			</p>
+			<p id="line">- - - -</p>
+			<span className="important" >	
+				프로필
+			</span>
+			
+			<p className="many-article">
+				자기소개, 경력, 학습 로드맵등의 개인설명이 담겨있습니다.<br/>
+				CSS 연습, JS 로직과 React 라이브러리 응용이 주목적입니다.
+			</p>
+			<p id="line">- - - -</p>			
+			<span className="important" >	
+				웹 모션 그래픽
+			</span>
+
+			<p className="many-article">
+				HTML5로 구현한 바닐라 프로그래밍 결과물 2개가 게시되어 있습니다<br/>
+				JavaScript 위주의 로직입니다.
+			</p>
+			<p id="line">- - - -</p>
+			<span className="important" >	
+				Python
+			</span>
+			<p className="many-article">
+				Python으로 제작된 딥러닝 모델, 웹기능들이 게시되어 있습니다. 
+			</p>
+			<p id="line">- - - -</p>
+		</div>
+	</div>
 	);
 	
 }
@@ -48,9 +82,10 @@ root.render(
     <BrowserRouter>
     {lact}
       <Routes>
-       <Route exact path="/Portfolio-Profile_Website/" element={<Lobby />}></Route>
-       <Route exact path="/Portfolio-Profile_Website/*" element={<App />}></Route>
-       <Route exact path="/Web-Motion-Graphic/*" element={<Graphicdesign />}></Route>	
+       <Route exact path="Portfolio-Profile_Website/" element={<Lobby />}></Route>
+       <Route exact path="Portfolio-Profile_Website/*" element={<App />}></Route>
+       <Route exact path="Web-Motion-Graphic/*" element={<Graphicdesign />}></Route>	
+       <Route exact path="Python/*" element={<Python />}></Route>	
 	  </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ new-profile-web/src/pythonmain.js
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+import './CSS/Pythonmain.css'
+
+
+
+function Python(){
+	
+	return(
+		<div id="Container">
+			<div id="App-Python-Main">
+				아직 준비중
+			</div>
+		</div>
+	);
+}
+
+
+export default Python;
